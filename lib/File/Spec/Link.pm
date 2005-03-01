e7d26d30e74fab849bfe671b4f9877dbe5d95702
Robin Barker <RMBarker@cpan.org>
2005-03-01 13:06:25 +0000

    Import of RMBARKER/File-Copy-Link-0.061 from CPAN.
    gitpan-cpan-distribution: File-Copy-Link
    gitpan-cpan-version:      0.061
    gitpan-cpan-path:         RMBARKER/File-Copy-Link-0.061.tar.gz
    gitpan-cpan-author:       RMBARKER
    gitpan-cpan-maturity:     released

--- lib/File/Spec/Link.pm
+++ lib/File/Spec/Link.pm
@@ -5,54 +5,51 @@ use warnings;
 
 use File::Spec ();
 our @ISA = qw(File::Spec);
-our $VERSION = '0.04';
+our $VERSION = 0.04_1;
 
+# over-ridden class method - just a debugging wrapper
+# 
+sub canonpath { 
+    my($spec, $path) = @_;
+    return $spec->SUPER::canonpath($path) if $path;
+    require Carp;
+    Carp::cluck( "canonpath: ", 
+		defined $path ? "empty path" : "path undefined"  
+    );
+    return $path;
+}
+sub catdir { my $spec = shift; @_ ? $spec->SUPER::catdir(@_) : $spec->curdir }
+
+# new class methods - implemented via objects
+# 
 sub linked { 
-    my($spec, $link) = @_;
-    my $read = readlink $link;
-    return unless defined $read;
-    return $spec->relative_to_file($read,$link);
+    my $self = shift -> new(@_); 
+    return unless $self -> follow; 
+    $self -> path; 
 }
- 
-sub relative_to_file {
-    my($spec, $path, $file) = @_;
-    return $path if $spec->file_name_is_absolute($path);
-    return unless defined(my $dir = $spec->chopfile($file));
-    return $spec->catdir($dir,$path);
+sub resolve { 
+    my $self = shift -> new(@_); 
+    return unless $self -> resolved; 
+    $self -> path; 
 }
-
-sub splitlast {
-    my($spec, $dir) = @_;
-    my @path = $spec->splitdir($dir);
-    my $found = scalar @path;
-    while( --$found ) { last if length $path[$found] } 
-    return ($spec->catdir(@path[0..$found-1]), $path[$found]);
+sub resolve_all { 
+    my $self = shift -> new(@_); 
+    return unless $self -> resolvedir; 
+    $self -> path; 
 }
-
+sub relative_to_file { 
+    my($spec, $path) = splice @_, 0, 2;
+    my $self = $spec -> new(@_); 
+    return unless $self -> relative($path);
+    $self -> path;
+}
 sub chopfile {
-    my($spec, $file) = @_;
-    my($vol, $dir);
-    ($vol, $dir, $file) = $spec->splitpath($file);	
-						# discard file component
-    unless( length $file ) {
-	($dir, $file) = $spec->splitlast($dir);
-	die unless length $file;
-    }
-    return $spec->catpath($vol, $dir, '')	if length $vol;
-    return $dir					if length $dir;
-    return $spec->curdir;
+    my $self = shift -> new(@_);
+    return $self -> path if length($self -> chop); 
 }
 
-sub resolve {
-    my($spec, $file) = @_;
-    my %seen;
-    while( -l $file ) {
-	return if $seen{$spec->canonpath($file)}++;
-	return unless defined($file = $spec->linked($file));
-    }
-    return $file;
-}
-
+# other new class methods - implemented via Cwd
+# 
 sub full_resolve {
     my($spec, $file) = @_;
     my $path = $spec->resolve_path($file);
@@ -66,42 +63,134 @@ sub resolve_path {
     return $spec->file_name_is_absolute($file)
 	    ? $path : $spec->abs2rel($path);
 } 
-    
-sub resolve_all {
-    my($spec, $file) = @_;
-    my %seen;
-    while( -l $file ) {
-	return if $seen{$spec->canonpath($file)}++;
-	return unless defined($file = $spec->linked($file));
+
+# old class method - not needed
+# 
+sub splitlast { 
+    my $self = shift -> new(@_);
+    my $last = $self -> chop;
+    return ($self -> path, $last);
+}
+
+# object methods: 
+# 	constructor methods	new
+# 	access methods		path, canonical, vol, dir 
+# 	updating methods	add, pop, push, split, chop
+# 				relative, follow, resolved, resolvedir  
+
+sub new { 
+    my $self = bless { }, shift; 
+    $self -> split(shift) if @_; 
+    $self; 
+}
+sub path { 
+    my $self = shift; 
+    $self -> catpath( $self->vol, $self->dir, '' ); 
+}
+sub canonical { my $self = shift; $self -> canonpath( $self -> path ); }
+sub vol { my $vol = shift->{vol}; return defined $vol ? $vol : '' } 
+sub dir { my $self = shift; $self -> catdir( $self -> dirs ); }
+sub dirs { my $dirs = shift->{dirs}; return $dirs ? @$dirs : () }
+	
+sub add {
+    my($self, $file) = @_;
+    if( $file eq $self -> curdir ) { }
+    elsif( $file eq $self -> updir ) { $self -> pop }
+    else { $self -> push($file); }
+}
+sub pop {
+    my $self = shift;
+    my @dirs = $self -> dirs;
+    if( !@dirs or $dirs[-1] eq $self -> updir ) {
+	push @{$self->{dirs}}, $self -> updir;
     }
-    my($vol, $dir, @temp);
-    ($vol, $dir, $file) = $spec->splitpath($file);
-    while( length $dir ) {
-	my $path = $spec->catpath($vol, $dir, '');
-	if( -l $path ) {
-	    return if $seen{$spec->canonpath($path)}++;
-	    return unless defined($path = $spec->linked($path));
-            ($vol, $dir) = $spec->splitpath($path, 1);
-	}
-	else {
-            ($dir, my $last) = $spec->splitlast($dir);	
-	    unshift @temp, $last;
-	}
-    }
-    my @path;
-    for my $path (@temp) {
- 	if( $path eq $spec->curdir ) { next if @path } 
-	elsif( $path eq $spec->updir ) {
-	    if( @path and $path[-1] ne $spec->updir ) {
-		pop @path; next 
-	    }
-	}
-	push @path, $path
+    elsif( length $dirs[-1] and $dirs[-1] ne $self -> curdir) {
+	CORE::pop @{$self->{dirs}}
     }	
-    $spec->catpath($vol, $spec->catdir(@path), $file);
+    else {
+	require Carp;
+	Carp::cluck( "Can't go up from ", 
+			length $dirs[-1] ? $dirs[-1]: "empty dir"
+	);
+    }
+}
+
+sub push {
+    my $self = shift;
+    my $file = shift;
+    CORE::push @{$self->{dirs}}, $file if length $file;
+}
+sub split {
+    my($self, $path) = @_;
+    my($vol, $dir, $file) = $self->splitpath($path, 1);
+    $self->{vol} = $vol;
+    $self->{dirs} = [ $self->splitdir($dir) ];
+    $self->push($file);
+}
+sub chop {
+    my $self = shift;
+    my $dirs = $self->{dirs};
+    my $file = '';
+    while( @$dirs ) {
+	last if @$dirs == 1 and not length $dirs->[0];	# path = '/'
+	last if length($file = CORE::pop @$dirs);
+    }
+    $file;    
+}    
+    
+sub follow {
+    my $self = shift;
+    my $path = $self -> path;
+    my $link = readlink $self->path;
+    return $self->relative($link) if defined $link;
+    require Carp;
+    Carp::confess(
+	"Can't readlink ", $self->path, 
+    	" : ", 
+	(-l $self->path ? "but it is" : "not"), 
+	"a link"
+    );
+}
+ 
+sub relative {
+    my($self, $path) = @_;
+    unless( $self->file_name_is_absolute($path) ) {
+	return unless length($self->chop);
+	$path = $self->catdir($self->path, $path);
+    }
+    # what we want to do here is just set $self->{path}
+    # to be read by $self->path; but would need to 
+    # unset $self->{path} whenever it becomes invalid
+    $self->split($path);
+    1;
+}
+
+sub resolved {
+    my $self = shift;
+    my $seen = @_ ? shift : {};
+    while( -l $self->path ) {
+	return if $seen->{$self->canonical}++;
+	return unless $self->follow;
+    }
+    1;
+}
+
+sub resolvedir {
+    my $self = shift;
+    my $seen = @_ ? shift : {};
+    my @path;
+    while( 1 ) {
+	return unless $self->resolved($seen);
+	my $last = $self->chop;
+	last unless length $last;
+	unshift @path, $last;
+    }
+    $self->add($_) for @path;    
+    1;
 }
 
 1;
+
 __END__
 # Below is stub documentation for your module. You'd better edit it!
 
@@ -182,7 +271,7 @@ Robin Barker, E<lt>Robin.Barker@npl.co.ukE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2003 by Robin Barker
+Copyright 2003, 2005 by Robin Barker
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself. 
